3d4245589744b52e8d9cce5deeb9c8e97e49ba0b
bobikenobi12 <borislav1207@gmail.com>
2023-08-07 20:18:07 +0300

    add dockerfile for running app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,22 +6,19 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage";
 import FirstPage from "./pages/FirstPage";
 
-
 function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/" element={<FirstPage/>} />
-				<Route path="/sign-up/">
-					<Route
-						path="student"
-						element={<SignUpPage role={Role.STUDENT} />}
-					/>
-					<Route
-						path="instructor"
-						element={<SignUpPage role={Role.INSTRUCTOR} />}
-					/>
-				</Route>
+				<Route path="/" element={<FirstPage />} />
+				<Route
+					path="/sign-up/student"
+					element={<SignUpPage role={Role.STUDENT} />}
+				/>
+				<Route
+					path="/sign-up/instructor"
+					element={<SignUpPage role={Role.INSTRUCTOR} />}
+				/>
 				<Route path="/sign-in" element={<SignInPage />} />
 				<Route path="*" element={<h1>Not found</h1>} />
 			</Routes>
