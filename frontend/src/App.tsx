755a8ccd3b6373c74978b9eb4e35ce076002e4cf
bobikenobi12 <borislav1207@gmail.com>
2023-08-08 16:11:03 +0300

    add route guards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,10 +8,16 @@ import HomePage from "./pages/HomePage";
 import TermsOfService from "./pages/TermsOfService";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 
+// Guards
+// import RequireAuth from "./utils/RequireAuth";
+import PublicOutlet from "./utils/PublicOutlet";
+
 function App() {
 	return (
 		<>
 			<Routes>
+				<Route path="/sign-up/*" element={<PublicOutlet />} />
+				<Route path="/sign-in" element={<PublicOutlet />} />
 				<Route path="/" element={<HomePage />} />
 				<Route
 					path="/sign-up/student"
@@ -22,8 +28,8 @@ function App() {
 					element={<SignUpPage role={Role.TEACHER} />}
 				/>
 				<Route path="/sign-in" element={<SignInPage />} />
-				<Route path="/terms-of-service" element={<TermsOfService/>}/>
-				<Route path="/privacy-policy" element={<PrivacyPolicy/>}/>
+				<Route path="/terms-of-service" element={<TermsOfService />} />
+				<Route path="/privacy-policy" element={<PrivacyPolicy />} />
 				<Route path="*" element={<h1>Not found</h1>} />
 			</Routes>
 		</>
