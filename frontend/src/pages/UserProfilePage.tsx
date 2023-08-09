b7385ac0067f7257d5eb5d377af25190fb202623
Cvetelina5566 <tsvete.pete@gmail.com>
2023-08-09 16:40:54 +0300

    Update Home page

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,4 +1,22 @@
-import {Text,Center,Image,Card,CardBody,Badge,Box,Container,Stack, StackDivider} from '@chakra-ui/react'
+import {
+        Text,
+        Center,
+        Image,
+        Card,
+        CardBody,
+        Badge,
+        Box,
+        Container,
+        Stack,
+        StackDivider,
+        Accordion,  
+        AccordionItem,
+        AccordionButton,
+        AccordionPanel,
+        AccordionIcon,
+        Button,
+        } from '@chakra-ui/react';
+        
 
 export default function UserProfilePage(){
 return(
@@ -52,6 +70,61 @@ return(
     </CardBody>
 </Card>
 </Container>
+
+<Container  ml='835px' mt='-400px'>
+<Text fontSize='3xl' mb='30px' color='#6065EA'>Course List: </Text>
+<Box overflowX="hidden" overflowY="scroll" h='330px' w='550px'>
+    <Card mr="15px" mt="20px">
+    <Stack>
+    <CardBody pt="10px" pb="10px" pl="20px" pr="10px">
+<Accordion defaultIndex={[0]} allowMultiple>
+    <AccordionItem>
+        <h2>
+            <AccordionButton>
+                <Box as='span' flex='1' textAlign='left' fontSize='xl'>
+                Deep Learning-12.08.2023-15.10.2023
+                </Box>
+                <AccordionIcon />
+            </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</p>
+        <p>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
+        <p>veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
+        commodo consequat.
+        <Button colorScheme='purple' size='sm' ml='300px' mb='10px'>
+            View course page
+        </Button>
+        </AccordionPanel>
+    </AccordionItem>
+    <AccordionItem>
+        <h2>
+            <AccordionButton>
+                <Box as='span' flex='1' textAlign='left' fontSize='xl' w='550px'>
+                Java Script-10.03.2023-12.06.2023
+                </Box>
+                <AccordionIcon />
+            </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+        <Text>
+        <p>Lire uso aca pago por dijo reia toco. Callar atenta mimada mil dio.</p> 
+        <p>Doy una dia clavo salir local gacha grima guapa. Eh va pegado oh famosa alcoba ch bebida. </p>
+        <p>Las egoismo tan redondo ovalado rio fin.</p>
+        <p>Propiedad caprichos en tendencia al prometido. Hembra grande yo no acabar.</p>
+        </Text>
+        <Button colorScheme='purple' size='sm' ml='300px' mb='10px'>
+            View course page
+        </Button>
+        </AccordionPanel>
+    </AccordionItem>
+</Accordion>
+</CardBody>
+</Stack>
+</Card>
+</Box>
+</Container>
+
     </>
 )
 }
