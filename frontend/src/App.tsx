27abf378f4d2e5bd6f34aceb338e696f637f3b7f
bobikenobi12 <borislav1207@gmail.com>
2023-08-10 13:42:36 +0300

    update styling for profile page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { Role } from "./features/auth/authApiSlice";
 
 // Pages
 import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
 import HomePage from "./pages/HomePage";
 import TermsOfService from "./pages/TermsOfService";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -12,13 +13,12 @@ import UserProfilePage from "./pages/UserProfilePage";
 // import RequireAuth from "./utils/RequireAuth";
 import PublicOutlet from "./utils/PublicOutlet";
 
-
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path="/sign-up/*" element={<PublicOutlet />} />
-				<Route path="/sign-in" element={<PublicOutlet />} />
+				<Route path="/sign-in/*" element={<PublicOutlet />} />
 				<Route path="/" element={<HomePage />} />
 				<Route
 					path="/sign-up/student"
@@ -28,9 +28,10 @@ function App() {
 					path="/sign-up/teacher"
 					element={<SignUpPage role={Role.TEACHER} />}
 				/>
+				<Route path="/sign-in" element={<SignInPage />} />
 				<Route path="/terms-of-service" element={<TermsOfService />} />
 				<Route path="/privacy-policy" element={<PrivacyPolicy />} />
-				<Route path="/user-profile" element={<UserProfilePage/>}/>
+				<Route path="/user-profile" element={<UserProfilePage />} />
 				<Route path="*" element={<h1>Not found</h1>} />
 			</Routes>
 		</>
